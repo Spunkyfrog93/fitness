935ab0d7081153ee5ada21a26588f2fe7d48da60
Spunkyfrog93 <60330400+Spunkyfrog93@users.noreply.github.com>
2020-06-02 18:57:19 -0400

    fitness routes

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local workout database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI)
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+})
